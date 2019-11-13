1cfbf830ad6a1b1e9fccf3bfa4b0bb92b79cd750
Krishan Kant <krish.kant.47@gmail.com>
2019-11-13 23:21:31 +0530

    Assets added

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text message;
+    //public Text input1;
+    private InputField input;
+
+    private void Awake()
+    {
+        input = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+    string ans = "history is written by victors";
+    public void GetInput(string guess)
+    {
+        //Debug.Log(guess);
+        if(guess.ToLower() == ans)
+        {
+            //Debug.Log("AREY TUM KRISHAN HO KYA?");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            Debug.Log("Bhak sala chutiya !!");
+            message.text = "TRY HARDER...";
+        }
+        input.text = "";
+    }
+}
---
+++ Assets/GameManager1.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager1 : MonoBehaviour
+{
+    public void LoadNext()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
